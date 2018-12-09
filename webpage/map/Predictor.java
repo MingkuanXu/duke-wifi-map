e976cf0ce2b59ec27d1c9e6202aea76ca2dfe780
Charles Lyu <liang.lyu@duke.edu>
2018-12-09 17:19:47 -0500

    Move map.html

--- webpage/map/Predictor.java
+++ webpage/map/Predictor.java
@@ -1,8 +1,15 @@
 
 public class Predictor {
 
+    static String XML_PATH = "webpage/src/data.xml";
+
     public Predictor() {
-        
+
+    }
+
+    private void train() {
+        // Placeholder, do nothing
+        // TODO
     }
 
     public double getSignal(double lat, double lon) {
