47885afabf997dd93d3a303ca077ff31e253e672
YUNHAO QING <yq50@duke.edu>
2018-12-09 14:47:23 -0500

    mmm

--- python/DukeDateParser.py
+++ python/DukeDateParser.py
@@ -1,7 +1,8 @@
 from xml.dom.minidom import Document
+import random
+
 
 def parse_csv(fileName):
-
     datas = []
     f = open(fileName, 'r')
     line = f.readline() # Ignore the first line
@@ -9,13 +10,16 @@ def parse_csv(fileName):
         # Read every other line starting from the second line.
         line = f.readline()
         if (line != ''):
-            myList = line.split(',')
-            wifiName = myList[0][1:-1]
-            wifiSignal = myList[3][1:-1]
-            wifiTime = myList[-3][1:-1]
-            wifiLongitude = myList[-2][1:-1]
-            wifiLatitude = myList[-1][1:-2]
-            if (wifiLongitude != '' and wifiLatitude != ''):
+            myList = line.split('","')
+            wifiName = myList[0][1:]
+            wifiSignal = random.random() * 5
+            #wifiSignal = myList[3]
+            wifiTime = myList[-3]
+            wifiLongitude = myList[-2]
+            wifiLatitude = myList[-1][:-2]
+            if (wifiLongitude != '' and wifiLatitude != '' and
+            (wifiName == 'DukeOpen' or wifiName == 'Dukeblue'
+            or wifiName == 'DukeVisitor' or wifiName == 'eduroam')):
                 data = []
                 data.append(wifiName)
                 data.append(wifiSignal)
@@ -26,25 +30,6 @@ def parse_csv(fileName):
         line = f.readline()
     f.close()
     return datas
-
-def write_into_xml(datas):
-    """
-    Write the latitudes and longtitudes to the file NYCDatabase.xml in the form
-    of:
-    <hotspots>
-        <location>
-            <latitude> latitude1 </latitude>
-            <longtitude> longtitude1 </longtitude>
-        </location>
-        <location>
-            ...
-        </location>
-        ...
-    <hotspots>
-    """
-    doc = Document()
-    hotspots = doc.createElement("hotspots")
-    doc.appendChild(datas)
 
 def write_into_xml(datas):
 
@@ -62,11 +47,11 @@ def write_into_xml(datas):
 
         signal = doc.createElement("signal")
         location.appendChild(signal)
-        signal.appendChild(doc.createTextNode(data[1]))
+        signal.appendChild(doc.createTextNode(str(data[1])))
 
         time = doc.createElement("time")
         location.appendChild(time)
-        time.appendChild(doc.createTextNode(data[1]))
+        time.appendChild(doc.createTextNode(data[2]))
 
         latitude = doc.createElement("latitude")
         location.appendChild(latitude)
